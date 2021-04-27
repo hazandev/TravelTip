1850e31a813d7a34041143352fac9d63197b404a
harel hazan <hazandev@gmail.com>
2021-04-27 19:10:24 +0300

    css

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -11,6 +11,7 @@ let gCurrPos;
 var gMap;
 let labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let labelIndex = 0;
+
 function initMap(lat = 32.0749831, lng = 34.9120554) {
     console.log('InitMap');
     return _connectGoogleApi()
